4fd095dd6d92df6da47176e95d698cc32e62b776
GJW <1875944372@qq.com>
2025-05-21 15:50:48 +0800

    login

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage';
+import SeedPage from './Pages/SeedPage'; 
+import UserProfile from './Pages/UserProfile'; 
+import SalePage from './Pages/SalePage'; 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/user" element={<UserProfile />} />
+            <Route path="/seed" element={<SeedPage />} /> 
+            <Route path="/sale" element={<SalePage />} />
+
+          </Routes>
+        </main>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
